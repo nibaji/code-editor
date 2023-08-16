318f6b79797abc83e529ec7f76285936ef20f51a
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2023-08-17 01:51:58 +0530

    init simple buggy editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Editor from "@monaco-editor/react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState<string | undefined>();
+	const [output, setOutput] = useState("");
+
+	console.log = function (message) {
+		console.info();
+		console.info(message);
+		if (message) {
+			if (typeof message === "object") {
+				const stringifiedMessage = JSON.stringify(message);
+				setOutput(output + "\n" + stringifiedMessage);
+			} else setOutput(output + "\n" + message);
+		}
+	};
+
+	const onChange = React.useCallback((value: string | undefined) => {
+		setInput(value);
+	}, []);
+
+	const onRun = () => {
+		if (input) {
+			(() => {
+				try {
+					eval(input);
+				} catch (error) {
+					console.log({ error });
+				}
+			})();
+		}
+	};
+
+	const onClear = () => setOutput("");
+
+	return (
+		<div className="App">
+			<header className="App-header">JS Code Editor</header>
+
+			<Editor
+				language="javascript"
+				height="60vh"
+				onChange={onChange}
+				theme="vs-dark"
+			/>
+			<button onClick={onRun}>Run</button>
+			<button onClick={onClear}>Clear Output</button>
+			{/* <pre>{output}</pre> */}
+			<Editor value={output} theme="vs-dark" height="30vh" />
+		</div>
+	);
 }
 
 export default App;
